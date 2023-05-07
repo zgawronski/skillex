4b1fd12dec18e6cae0247a353e9deffdb21b582e
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2023-05-07 14:20:49 +0200

    table view in progress

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,25 +1,50 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
-import { Wrapper, SkillFrame, SkillLabel } from './Table.styles';
+import { Wrapper, SkillFrame, SkillLabel, SkillBox, WorkerFrame, WorkerBox, WorkerLabel, LevelFrame, LevelBox, LevelMarker } from './Table.styles';
 
 export const Table = () => {
   const [skills, setSkills] = useState([]);
+  const [workers, setWorkers] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:8000/skills')
+    fetch('http://localhost:3000/skills')
       .then((res) => res.json())
       .then((data) => setSkills(data));
   }, []);
 
-  const skillsBox = skills.map((skill) => skill);
+  useEffect(() => {
+    fetch('http://localhost:3000/workers')
+      .then((res) => res.json())
+      .then((data) => setWorkers(data));
+  }, []);
+
+  const skillsArray = skills.map((skill) => skill);
+  const workersArray = workers.map((worker) => worker);
+  console.log(workersArray);
 
   return (
     <Wrapper>
       <SkillFrame>
-        {skillsBox.map((skill, index) => (
-          <SkillLabel key={index}>{Object.values(skill)}</SkillLabel>
+        {skillsArray.map((skill, index) => (
+          <SkillBox style={{ order: index }} key={index}>
+            <SkillLabel key={index}>{Object.values(skill)}</SkillLabel>
+          </SkillBox>
         ))}
       </SkillFrame>
+      <WorkerFrame>
+        {workersArray.map((worker, index) => (
+          <WorkerBox style={{ order: index }} key={index}>
+            <WorkerLabel key={index}>{Object.values(worker)} </WorkerLabel>
+          </WorkerBox>
+        ))}
+      </WorkerFrame>
+      {skillsArray.map((skillsMarkers, index) => (
+        <LevelFrame key={index}>
+          <LevelBox tyle={{ order: index }} key={index}>
+            <LevelMarker key={index} />
+          </LevelBox>
+        </LevelFrame>
+      ))}
     </Wrapper>
   );
 };
--- src/components/Table/Table.styles.jsx
+++ src/components/Table/Table.styles.jsx
@@ -9,14 +9,87 @@ export const SkillFrame = styled.div`
 
   position: absolute;
   width: 1211px;
+  height: 125px;
   left: 397px;
-  top: 138px;
+  top: 300px;
+`;
+
+export const SkillBox = styled.div`
+  width: 125.24px;
+  height: 120.75px;
+  flex: none;
+  flex-grow: 0;
+  margin: 0px -35px;
 `;
 export const SkillLabel = styled.div`
-  width: 400px;
-  height: 25px;
+  width: 150px;
+  height: 24px;
+  padding: 3px;
+  padding-left: 5px;
+  left: 0;
+  right: -19.77%;
+  top: 85.6%;
+  bottom: -5.47%;
   font-size: ${({ theme }) => theme.fontSize.s};
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.darkBlue};
   background: ${({ theme }) => theme.colors.formBackground};
-  border: 1px solid ${({ theme }) => theme.colors.blue};
+  border: 1px solid ${({ theme }) => theme.colors.formStroke};
   transform: rotate(-43.55deg);
+`;
+
+export const WorkerFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  position: absolute;
+  width: 150px;
+  height: 704px;
+  left: 200px;
+  top: 400px;
+`;
+export const WorkerBox = styled.div``;
+
+export const WorkerLabel = styled.div`
+  width: 150px;
+  height: 24px;
+  margin-bottom: 10px;
+  padding: 3px;
+  padiing-left: 5px;
+  font-size: ${({ theme }) => theme.fontSize.s};
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.darkBlue};
+
+  background: ${({ theme }) => theme.colors.formBackground};
+  border: 1px solid ${({ theme }) => theme.colors.formStroke};
+`;
+
+export const LevelFrame = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  position: relative;
+  width: 1115px;
+  height: 24px;
+`;
+
+export const LevelBox = styled.div`
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  left: 451px;
+  top: 0px;
+`;
+export const LevelMarker = styled.div`
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: ${({ theme }) => theme.colors.formBackground};
+  mix-blend-mode: normal;
+  border: 1px solid ${({ theme }) => theme.colors.formStroke};
+  border-radius: 15px;
 `;
