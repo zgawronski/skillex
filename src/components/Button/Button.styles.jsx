7caea23032a2c0a6e0897e5ef290cfa014ac137a
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2023-05-17 09:36:09 +0200

    button redesign

--- src/components/Button/Button.styles.jsx
+++ src/components/Button/Button.styles.jsx
@@ -3,14 +3,12 @@ import styled from 'styled-components';
 const StyledButton = styled.button`
   width: 7.5em;
   height: 1.875em;
-  background: ${({ theme }) => theme.colors.formStroke};
+  background: ${({ theme }) => theme.colors.skillLvl3};
   border: 1px solid ${({ theme }) => theme.colors.skillLvl3};
-  border-radius: 0.9375rem;
   cursor: pointer;
   box-shadow: 0 0.1875rem 0.5rem rgba(0, 0, 0, 0.3);
   font-size: ${({ theme }) => theme.fontSize.s};
-  color: ${({ theme }) => theme.colors.skillLvl3};
-
+  color: ${({ theme }) => theme.colors.white};
   :active {
     transform: translateY(0.25rem);
     outline: none;
