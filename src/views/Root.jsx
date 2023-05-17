542c32b6f8c704775074477feaa90c33d19da856
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2023-05-17 08:37:43 +0200

    LoginScreen

--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import { Wrapper } from './Root.styles';
 import { Dashboard } from './Dashboard';
+import { LoginScreen } from './LoginScreen';
 
 export const Root = () => {
   return (
     <Wrapper>
-      <Dashboard />
+      <Routes>
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </Wrapper>
   );
 };
