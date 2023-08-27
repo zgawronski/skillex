42e65f2968bb114384ca59d86b2a914fc63dddb8
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2023-08-28 00:09:23 +0200

    skill filter working

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -18,11 +18,16 @@ export const Table = () => {
   const [skilledWorkers, setSkilledWorkers] = useState([]);
   const [hasError, setHasError] = useState(false);
   const [searchInput, setSearchInput] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [skillsFilter, setSkillsFilter] = useState();
   // fetching data
   useEffect(() => {
     fetch('http://localhost:3000/skilledWorkers')
       .then((res) => res.json())
-      .then((data) => setSkilledWorkers(data))
+      .then((data) => {
+        setSkilledWorkers(data);
+        setSkillsFilter(data.map((skills) => Object.values(skills)));
+      })
       .catch(() => setHasError(true));
   }, []);
   // console.log(skilledWorkers);
@@ -30,9 +35,7 @@ export const Table = () => {
   const skillsArray = skillWorkersArray.map((skills) => Object.keys(skills));
   const skillsForArray = skillWorkersArray.map((skills) => Object.values(skills));
 
-  const [skillsFilter, setSkillsFilter] = useState(skillsForArray);
-
-  console.log(skillsFilter);
+  // console.log(skillsForArray);
 
   // handle search input
   const handleChange = (e) => {
@@ -41,12 +44,14 @@ export const Table = () => {
     setSearchInput(searchValue);
   };
 
-  console.log(hasError);
+  console.log('Error: ' + hasError);
   // filtering
   useEffect(() => {
-    if (searchInput.length != '')
+    setSkillsFilter(skillsForArray);
+    if (searchInput.length !== '')
       setSkillsFilter(skillsForArray.filter((searchItem) => searchItem.map((e) => e.toLowerCase()).includes(searchInput.toLowerCase())));
     if (searchInput.length == '') setSkillsFilter(skillsForArray);
+    setLoading(false);
   }, [searchInput]);
 
   return (
@@ -57,18 +62,24 @@ export const Table = () => {
 
           <Magnifier src="./img/magnifier.png" alt="magnifier" />
         </SearchInput>
-        {skillsFilter.map((worker, index) => (
-          <WorkerSkillWrapper key={index}>
-            <WorkerLabel style={{ order: index }} key={index + 100}>
-              {Object.values(worker[0])} {Object.values(worker[1])}
-            </WorkerLabel>
-            <LevelFrame key={index + 100000} style={{ order: index }}>
-              {skillsForArray[index].slice(2).map((skill, index) => (
-                <LevelMarker style={{ order: index }} name={skill} key={index + 1000}></LevelMarker>
-              ))}
-            </LevelFrame>
-          </WorkerSkillWrapper>
-        ))}
+        {loading ? (
+          'loading...'
+        ) : (
+          <>
+            {skillsFilter.map((worker, index) => (
+              <WorkerSkillWrapper key={index}>
+                <WorkerLabel style={{ order: index }} key={index + 100}>
+                  {Object.values(worker[0])} {Object.values(worker[1])}
+                </WorkerLabel>
+                <LevelFrame key={index + 100000} style={{ order: index }}>
+                  {skillsForArray[index].slice(2).map((skill, index) => (
+                    <LevelMarker style={{ order: index }} name={skill} key={index + 1000}></LevelMarker>
+                  ))}
+                </LevelFrame>
+              </WorkerSkillWrapper>
+            ))}
+          </>
+        )}
       </WorkerFrame>
       <SkillFrame>
         {skillsArray[0]?.slice(2).map((skill, index) => (
