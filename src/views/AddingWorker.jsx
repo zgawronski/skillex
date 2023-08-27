c3d57261f593e3c24df9a65fba475aeb11f5cae4
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2023-08-27 08:21:22 +0200

    creating a new view AddingWorker

---
+++ src/views/AddingWorker.jsx
@@ -0,0 +1,13 @@
+import { Header } from '../components/Header/Header';
+import { Table } from '../components/Table/Table';
+import { Description } from '../components/Description/Description';
+
+export const AddingWorker = () => {
+  return (
+    <div>
+      <Header />
+      <Table />
+      <Description />
+    </div>
+  );
+};
